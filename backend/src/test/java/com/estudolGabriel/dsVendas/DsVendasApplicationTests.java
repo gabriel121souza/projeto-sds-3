a0ab88087a7ac6432737e9a35dba3296e57c5ab3
gabriel121souza <gabriel121souza@gmail.com>
2021-05-25 12:50:00 -0300

    projeto criado

---
+++ backend/src/test/java/com/estudolGabriel/dsVendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudolGabriel.dsVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
